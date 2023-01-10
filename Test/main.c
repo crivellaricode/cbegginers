e37bdfe51a08d7fe7d682a180578bd2778581c33
Sidney Crivellari <crivellaricode@gmail.com>
2023-01-10 16:58:44 -0300

    comments

---
+++ Test/main.c
@@ -0,0 +1,15 @@
+/*
+Author: Sidney Crivellari
+Purpose: This program prints out my name to screen
+Date: 01/10/23
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // this code is displaying my name
+    printf("Hi, my name is Sidney\n");
+    return 0;
+}
