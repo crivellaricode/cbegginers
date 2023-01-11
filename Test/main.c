aa83fe4e5809f31f310fd63363be8d5e63482756
Sidney Crivellari <crivellaricode@gmail.com>
2023-01-11 08:20:33 -0300

    adding comment

--- Test/main.c
+++ Test/main.c
@@ -2,10 +2,9 @@
 Author: Sidney Crivellari
 Purpose: This program prints out my name to screen
 Date: 01/10/23
-
 */
 
-#include <stdio.h>
+#include <stdio.h>   // the include statement is a preprocessed directive
 
 int main()
 {
