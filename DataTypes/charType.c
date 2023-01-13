9a95df0ad0f60e21851793ab5919d3a2bd05d470
Sidney Crivellari <crivellaricode@gmail.com>
2023-01-13 11:16:37 -0300

    lesson 31 char

---
+++ DataTypes/charType.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char myCharacter = 'S';
+    char simbolChar = 64;
+
+    printf("myCharacter = %c and ASCII = %d\n", myCharacter, myCharacter);
+    printf("simbolChar = %c and ASCII = %d\n", simbolChar, simbolChar);
+    return 0;
+}
